61f2271d1a7f3d2b2eaeeb5dab7849ee476afd21
walt-instructor <staffauthor@outlook.com>
2021-06-07 23:56:10 -0700

    Add test code for ReturnDate30DaysInFuture_WhenProposedDateFallsOnWeekday, rename solution folder to RunTest

--- source/BrokerageLib.XUnit.Tests/PaymentDate/CalculateFuturePaymentDate.cs
+++ source/BrokerageLib.XUnit.Tests/PaymentDate/CalculateFuturePaymentDate.cs
@@ -3,8 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using SUT = BrokerageLib;
 
 namespace BrokerageLib.XUnit.Tests.PaymentDate {
-	class CalculateFuturePaymentDate {
+	public class CalculateFuturePaymentDate_Should {
+
+		[Fact]
+		public void ReturnDate30DaysInFuture_WhenProposedDateFallsOnWeekday() {
+
+			// arrange
+
+			var pd = new SUT.PaymentSystem.PaymentDate();
+			DateTime sampleDate = DateTime.Parse("7/6/2011");
+
+			// act
+			var resultWhichShouldBe30DaysLater = pd.CalculateFuturePaymentDate(sampleDate);
+
+			// assert
+			Assert.Equal(expected: sampleDate.AddDays(30), actual: resultWhichShouldBe30DaysLater);
+		}
 	}
 }
